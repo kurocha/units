49ac912a871ef8f7c63479f911c1e538d93a3ddd
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-15 21:54:19 +1300

    Modernize teapot config.

--- teapot.rb
+++ teapot.rb
@@ -1,6 +1,6 @@
 # Teapot v2.2.0 configuration generated at 2017-10-12 18:49:03 +1300
 
-required_version "2.0"
+required_version "3.0"
 
 # Project Metadata
 
@@ -23,38 +23,29 @@ end
 # Build Targets
 
 define_target 'units-library' do |target|
-	target.build do
-		source_root = target.package.path + 'source'
-		copy headers: source_root.glob('Units/**/*.{h,hpp}')
-		build static_library: 'Units', source_files: source_root.glob('Units/**/*.cpp')
-	end
-	
-	target.depends 'Build/Files'
-	target.depends 'Build/Clang'
-	
-	target.depends :platform
 	target.depends 'Language/C++14', private: true
 	
 	target.provides 'Library/Units' do
-		append linkflags [
-			->{install_prefix + 'lib/libUnits.a'},
-		]
+		source_root = target.package.path + 'source'
+		
+		library_path = build static_library: 'Units', source_files: source_root.glob('Units/**/*.cpp')
+		
+		append linkflags library_path
+		append header_search_paths source_root
 	end
 end
 
 define_target 'units-test' do |target|
-	target.build do |*arguments|
-		test_root = target.package.path + 'test'
-		
-		run tests: 'Units', source_files: test_root.glob('Units/**/*.cpp'), arguments: arguments
-	end
-	
 	target.depends 'Language/C++14', private: true
 	
 	target.depends 'Library/UnitTest'
 	target.depends 'Library/Units'
 	
-	target.provides 'Test/Units'
+	target.provides 'Test/Units' do |*arguments|
+		test_root = target.package.path + 'test'
+		
+		run tests: 'Units', source_files: test_root.glob('Units/**/*.cpp'), arguments: arguments
+	end
 end
 
 # Configurations
